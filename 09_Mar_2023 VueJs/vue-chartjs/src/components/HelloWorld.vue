dfc27c42c687307c070549022d6eb35d0e46d268
SanthoshGoud <saarasanthoshgoud@gmai.com>
2023-03-09 17:43:11 +0530

    Working on VueJS

---
+++ 09_Mar_2023 VueJs/vue-chartjs/src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+(async function() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
